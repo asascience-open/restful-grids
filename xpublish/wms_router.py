b3cd340dcbdda9109329413b610ab543d170f9fc
Matthew Iannucci <mpiannucci@gmail.com>
2022-05-06 17:35:50 -0400

    Stub out the minimum wms methods

--- xpublish/wms_router.py
+++ xpublish/wms_router.py
@@ -1,10 +1,53 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Request
+from xpublish.dependencies import get_dataset
+import xarray as xr
+import xml.etree.ElementTree as ET
 
 
 wms_router = APIRouter()
 
 
-@wms_router.get('/')
-def wms_root():
+def get_capabilities(dataset: xr.Dataset):
+    """
+    Return the WMS capabilities for the dataset
+    """
+
     return ''
+
+
+def get_map(dataset: xr.Dataset, query: dict):
+    """
+    Return the WMS map for the dataset and given parameters
+    """
+    return ''
+
+
+def get_feature_info(dataset: xr.Dataset, query: dict):
+    """
+    Return the WMS feature info for the dataset and given parameters
+    """
+    return ''
+
+
+def get_legend_graphic(dataset: xr.Dataset, query: dict):
+    """
+    Return the WMS legend graphic for the dataset and given parameters
+    """
+    return ''
+
+
+@wms_router.get('/')
+def wms_root(request: Request, dataset: xr.Dataset = Depends(get_dataset)):
+    query_params = request.query_params
+    method = query_params['request']
+    if method == 'GetCapabilities':
+        return get_capabilities(dataset)
+    elif method == 'GetMap':
+        return get_map(dataset, query_params)
+    elif method == 'GetFeatureInfo':
+        return get_feature_info(dataset, query_params)
+    elif method == 'GetLegendGraphic':
+        return get_legend_graphic(dataset, query_params)
+    else: 
+        raise HTTPException(status_code=404, detail=f"{method} is not a valid option for REQUEST")
